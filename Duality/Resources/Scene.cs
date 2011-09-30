c4de75ba4a2abe279379caa93196fcf7173ee77a
mail@fetzenet.de <mail@fetzenet.de@879bfe85-3099-8c59-a0db-7ea583510f3d>
2011-09-30 19:43:42 +0000

    git-svn-id: http://duality.googlecode.com/svn/trunk@171 879bfe85-3099-8c59-a0db-7ea583510f3d

--- Duality/Resources/Scene.cs
+++ Duality/Resources/Scene.cs
@@ -33,7 +33,7 @@ namespace Duality.Resources
 		/// <summary>
 		/// [GET] Returns the current physics world.
 		/// </summary>
-		internal static World CurrentPhysics
+		public static World CurrentPhysics
 		{
 			get { return physicsWorld; }
 		}
