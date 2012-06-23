807c9f27cf8e61651b1b8f899fffc079594648d6
mail@fetzenet.de <mail@fetzenet.de@879bfe85-3099-8c59-a0db-7ea583510f3d>
2012-06-23 07:43:57 +0000

    Minor bugfix in old DynamicLighting techdemo (starting scene)
    git-svn-id: http://duality.googlecode.com/svn/trunk@413 879bfe85-3099-8c59-a0db-7ea583510f3d

--- Other/TechDemos/DynamicLighting/Source/Code/CorePlugin/Properties/GameRes.cs
+++ Other/TechDemos/DynamicLighting/Source/Code/CorePlugin/Properties/GameRes.cs
@@ -7,6 +7,22 @@ namespace GameRes
 {
 	public static class Data {
 		public static class PerPixelLighting {
+			public static class SmoothAnim {
+				private static Duality.ContentRef<Duality.Resources.FragmentShader> _Light_FragmentShader;
+				public static Duality.ContentRef<Duality.Resources.FragmentShader> Light_FragmentShader { get { if (_Light_FragmentShader.IsExplicitNull) _Light_FragmentShader = Duality.ContentProvider.RequestContent<Duality.Resources.FragmentShader>(@"Data\PerPixelLighting\SmoothAnim\Light.FragmentShader.res"); return _Light_FragmentShader; }}
+				private static Duality.ContentRef<Duality.Resources.ShaderProgram> _Light_ShaderProgram;
+				public static Duality.ContentRef<Duality.Resources.ShaderProgram> Light_ShaderProgram { get { if (_Light_ShaderProgram.IsExplicitNull) _Light_ShaderProgram = Duality.ContentProvider.RequestContent<Duality.Resources.ShaderProgram>(@"Data\PerPixelLighting\SmoothAnim\Light.ShaderProgram.res"); return _Light_ShaderProgram; }}
+				private static Duality.ContentRef<Duality.Resources.VertexShader> _Light_VertexShader;
+				public static Duality.ContentRef<Duality.Resources.VertexShader> Light_VertexShader { get { if (_Light_VertexShader.IsExplicitNull) _Light_VertexShader = Duality.ContentProvider.RequestContent<Duality.Resources.VertexShader>(@"Data\PerPixelLighting\SmoothAnim\Light.VertexShader.res"); return _Light_VertexShader; }}
+				private static Duality.ContentRef<DynamicLighting.LightingTechnique> _Mask_LightingTechnique;
+				public static Duality.ContentRef<DynamicLighting.LightingTechnique> Mask_LightingTechnique { get { if (_Mask_LightingTechnique.IsExplicitNull) _Mask_LightingTechnique = Duality.ContentProvider.RequestContent<DynamicLighting.LightingTechnique>(@"Data\PerPixelLighting\SmoothAnim\Mask.LightingTechnique.res"); return _Mask_LightingTechnique; }}
+				public static void LoadAll() {
+					Light_FragmentShader.MakeAvailable();
+					Light_ShaderProgram.MakeAvailable();
+					Light_VertexShader.MakeAvailable();
+					Mask_LightingTechnique.MakeAvailable();
+				}
+			}
 			private static Duality.ContentRef<Duality.Resources.FragmentShader> _Light_FragmentShader;
 			public static Duality.ContentRef<Duality.Resources.FragmentShader> Light_FragmentShader { get { if (_Light_FragmentShader.IsExplicitNull) _Light_FragmentShader = Duality.ContentProvider.RequestContent<Duality.Resources.FragmentShader>(@"Data\PerPixelLighting\Light.FragmentShader.res"); return _Light_FragmentShader; }}
 			private static Duality.ContentRef<Duality.Resources.ShaderProgram> _Light_ShaderProgram;
@@ -16,6 +32,7 @@ namespace GameRes
 			private static Duality.ContentRef<DynamicLighting.LightingTechnique> _Mask_LightingTechnique;
 			public static Duality.ContentRef<DynamicLighting.LightingTechnique> Mask_LightingTechnique { get { if (_Mask_LightingTechnique.IsExplicitNull) _Mask_LightingTechnique = Duality.ContentProvider.RequestContent<DynamicLighting.LightingTechnique>(@"Data\PerPixelLighting\Mask.LightingTechnique.res"); return _Mask_LightingTechnique; }}
 			public static void LoadAll() {
+				SmoothAnim.LoadAll();
 				Light_FragmentShader.MakeAvailable();
 				Light_ShaderProgram.MakeAvailable();
 				Light_VertexShader.MakeAvailable();
@@ -23,6 +40,22 @@ namespace GameRes
 			}
 		}
 		public static class PerVertexLighting {
+			public static class SmoothAnim {
+				private static Duality.ContentRef<Duality.Resources.FragmentShader> _VertexLight_FragmentShader;
+				public static Duality.ContentRef<Duality.Resources.FragmentShader> VertexLight_FragmentShader { get { if (_VertexLight_FragmentShader.IsExplicitNull) _VertexLight_FragmentShader = Duality.ContentProvider.RequestContent<Duality.Resources.FragmentShader>(@"Data\PerVertexLighting\SmoothAnim\VertexLight.FragmentShader.res"); return _VertexLight_FragmentShader; }}
+				private static Duality.ContentRef<Duality.Resources.ShaderProgram> _VertexLight_ShaderProgram;
+				public static Duality.ContentRef<Duality.Resources.ShaderProgram> VertexLight_ShaderProgram { get { if (_VertexLight_ShaderProgram.IsExplicitNull) _VertexLight_ShaderProgram = Duality.ContentProvider.RequestContent<Duality.Resources.ShaderProgram>(@"Data\PerVertexLighting\SmoothAnim\VertexLight.ShaderProgram.res"); return _VertexLight_ShaderProgram; }}
+				private static Duality.ContentRef<Duality.Resources.VertexShader> _VertexLight_VertexShader;
+				public static Duality.ContentRef<Duality.Resources.VertexShader> VertexLight_VertexShader { get { if (_VertexLight_VertexShader.IsExplicitNull) _VertexLight_VertexShader = Duality.ContentProvider.RequestContent<Duality.Resources.VertexShader>(@"Data\PerVertexLighting\SmoothAnim\VertexLight.VertexShader.res"); return _VertexLight_VertexShader; }}
+				private static Duality.ContentRef<Duality.Resources.DrawTechnique> _VertexLightMask_DrawTechnique;
+				public static Duality.ContentRef<Duality.Resources.DrawTechnique> VertexLightMask_DrawTechnique { get { if (_VertexLightMask_DrawTechnique.IsExplicitNull) _VertexLightMask_DrawTechnique = Duality.ContentProvider.RequestContent<Duality.Resources.DrawTechnique>(@"Data\PerVertexLighting\SmoothAnim\VertexLightMask.DrawTechnique.res"); return _VertexLightMask_DrawTechnique; }}
+				public static void LoadAll() {
+					VertexLight_FragmentShader.MakeAvailable();
+					VertexLight_ShaderProgram.MakeAvailable();
+					VertexLight_VertexShader.MakeAvailable();
+					VertexLightMask_DrawTechnique.MakeAvailable();
+				}
+			}
 			private static Duality.ContentRef<Duality.Resources.FragmentShader> _VertexLight_FragmentShader;
 			public static Duality.ContentRef<Duality.Resources.FragmentShader> VertexLight_FragmentShader { get { if (_VertexLight_FragmentShader.IsExplicitNull) _VertexLight_FragmentShader = Duality.ContentProvider.RequestContent<Duality.Resources.FragmentShader>(@"Data\PerVertexLighting\VertexLight.FragmentShader.res"); return _VertexLight_FragmentShader; }}
 			private static Duality.ContentRef<Duality.Resources.ShaderProgram> _VertexLight_ShaderProgram;
@@ -32,6 +65,7 @@ namespace GameRes
 			private static Duality.ContentRef<Duality.Resources.DrawTechnique> _VertexLightMask_DrawTechnique;
 			public static Duality.ContentRef<Duality.Resources.DrawTechnique> VertexLightMask_DrawTechnique { get { if (_VertexLightMask_DrawTechnique.IsExplicitNull) _VertexLightMask_DrawTechnique = Duality.ContentProvider.RequestContent<Duality.Resources.DrawTechnique>(@"Data\PerVertexLighting\VertexLightMask.DrawTechnique.res"); return _VertexLightMask_DrawTechnique; }}
 			public static void LoadAll() {
+				SmoothAnim.LoadAll();
 				VertexLight_FragmentShader.MakeAvailable();
 				VertexLight_ShaderProgram.MakeAvailable();
 				VertexLight_VertexShader.MakeAvailable();
@@ -65,8 +99,10 @@ namespace GameRes
 		public static Duality.ContentRef<Duality.Resources.Material> LightShip_Material { get { if (_LightShip_Material.IsExplicitNull) _LightShip_Material = Duality.ContentProvider.RequestContent<Duality.Resources.Material>(@"Data\LightShip.Material.res"); return _LightShip_Material; }}
 		private static Duality.ContentRef<Duality.Resources.Material> _LightShip3_Material;
 		public static Duality.ContentRef<Duality.Resources.Material> LightShip3_Material { get { if (_LightShip3_Material.IsExplicitNull) _LightShip3_Material = Duality.ContentProvider.RequestContent<Duality.Resources.Material>(@"Data\LightShip3.Material.res"); return _LightShip3_Material; }}
-		private static Duality.ContentRef<Duality.Resources.Material> _LightTest_Material;
-		public static Duality.ContentRef<Duality.Resources.Material> LightTest_Material { get { if (_LightTest_Material.IsExplicitNull) _LightTest_Material = Duality.ContentProvider.RequestContent<Duality.Resources.Material>(@"Data\LightTest.Material.res"); return _LightTest_Material; }}
+		private static Duality.ContentRef<Duality.Resources.Material> _LightShipSmoothAnim_Material;
+		public static Duality.ContentRef<Duality.Resources.Material> LightShipSmoothAnim_Material { get { if (_LightShipSmoothAnim_Material.IsExplicitNull) _LightShipSmoothAnim_Material = Duality.ContentProvider.RequestContent<Duality.Resources.Material>(@"Data\LightShipSmoothAnim.Material.res"); return _LightShipSmoothAnim_Material; }}
+		private static Duality.ContentRef<Duality.Resources.Material> _LightShipVertexSmoothAnim_Material;
+		public static Duality.ContentRef<Duality.Resources.Material> LightShipVertexSmoothAnim_Material { get { if (_LightShipVertexSmoothAnim_Material.IsExplicitNull) _LightShipVertexSmoothAnim_Material = Duality.ContentProvider.RequestContent<Duality.Resources.Material>(@"Data\LightShipVertexSmoothAnim.Material.res"); return _LightShipVertexSmoothAnim_Material; }}
 		private static Duality.ContentRef<Duality.Resources.Pixmap> _normalBall_Pixmap;
 		public static Duality.ContentRef<Duality.Resources.Pixmap> normalBall_Pixmap { get { if (_normalBall_Pixmap.IsExplicitNull) _normalBall_Pixmap = Duality.ContentProvider.RequestContent<Duality.Resources.Pixmap>(@"Data\normalBall.Pixmap.res"); return _normalBall_Pixmap; }}
 		private static Duality.ContentRef<Duality.Resources.Texture> _normalBall_Texture;
@@ -87,6 +123,8 @@ namespace GameRes
 		public static Duality.ContentRef<Duality.Resources.Pixmap> ship_c_Pixmap { get { if (_ship_c_Pixmap.IsExplicitNull) _ship_c_Pixmap = Duality.ContentProvider.RequestContent<Duality.Resources.Pixmap>(@"Data\ship_c.Pixmap.res"); return _ship_c_Pixmap; }}
 		private static Duality.ContentRef<Duality.Resources.Texture> _ship_c_Texture;
 		public static Duality.ContentRef<Duality.Resources.Texture> ship_c_Texture { get { if (_ship_c_Texture.IsExplicitNull) _ship_c_Texture = Duality.ContentProvider.RequestContent<Duality.Resources.Texture>(@"Data\ship_c.Texture.res"); return _ship_c_Texture; }}
+		private static Duality.ContentRef<Duality.Resources.Texture> _ship_c_anim_Texture;
+		public static Duality.ContentRef<Duality.Resources.Texture> ship_c_anim_Texture { get { if (_ship_c_anim_Texture.IsExplicitNull) _ship_c_anim_Texture = Duality.ContentProvider.RequestContent<Duality.Resources.Texture>(@"Data\ship_c_anim.Texture.res"); return _ship_c_anim_Texture; }}
 		private static Duality.ContentRef<Duality.Resources.Pixmap> _ship_n_Pixmap;
 		public static Duality.ContentRef<Duality.Resources.Pixmap> ship_n_Pixmap { get { if (_ship_n_Pixmap.IsExplicitNull) _ship_n_Pixmap = Duality.ContentProvider.RequestContent<Duality.Resources.Pixmap>(@"Data\ship_n.Pixmap.res"); return _ship_n_Pixmap; }}
 		private static Duality.ContentRef<Duality.Resources.Texture> _ship_n_Texture;
@@ -105,7 +143,8 @@ namespace GameRes
 			LightBallVertex_Material.MakeAvailable();
 			LightShip_Material.MakeAvailable();
 			LightShip3_Material.MakeAvailable();
-			LightTest_Material.MakeAvailable();
+			LightShipSmoothAnim_Material.MakeAvailable();
+			LightShipVertexSmoothAnim_Material.MakeAvailable();
 			normalBall_Pixmap.MakeAvailable();
 			normalBall_Texture.MakeAvailable();
 			ship3_c_Pixmap.MakeAvailable();
@@ -116,6 +155,7 @@ namespace GameRes
 			ship3_s_Texture.MakeAvailable();
 			ship_c_Pixmap.MakeAvailable();
 			ship_c_Texture.MakeAvailable();
+			ship_c_anim_Texture.MakeAvailable();
 			ship_n_Pixmap.MakeAvailable();
 			ship_n_Texture.MakeAvailable();
 			ship_s_Pixmap.MakeAvailable();
