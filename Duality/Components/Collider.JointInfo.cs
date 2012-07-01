52473bc051ae9565907d55069feba8031b229b35
mail@fetzenet.de <mail@fetzenet.de@879bfe85-3099-8c59-a0db-7ea583510f3d>
2012-07-01 20:54:22 +0000

    Collider Joint WiP --> Added FixedDistanceJointInfo
    git-svn-id: http://duality.googlecode.com/svn/trunk@424 879bfe85-3099-8c59-a0db-7ea583510f3d

--- Duality/Components/Collider.JointInfo.cs
+++ Duality/Components/Collider.JointInfo.cs
@@ -206,14 +206,108 @@ namespace Duality.Components
 		}
 
 		/// <summary>
+		/// Constrains the Collider to obtain a fixed distance to a world coordinate
+		/// </summary>
+		[Serializable]
+		public sealed class FixedDistanceJointInfo : JointInfo
+		{
+			private	Vector2		localAnchor		= Vector2.Zero;
+			private	Vector2		worldAnchor		= Vector2.Zero;
+			private	float		dampingRatio	= 0.5f;
+			private	float		frequency		= 1.0f;
+			private	float		length			= 200.0f;
+
+
+			public override bool DualJoint
+			{
+				get { return false; }
+			}
+			/// <summary>
+			/// [GET / SET] The Colliders local anchor point.
+			/// </summary>
+			[EditorHintIncrement(1)]
+			public Vector2 LocalAnchor
+			{
+				get { return this.localAnchor; }
+				set { this.localAnchor = value; this.UpdateJoint(); }
+			}
+			/// <summary>
+			/// [GET / SET] The world anchor point to which the Collider will be attached.
+			/// </summary>
+			[EditorHintIncrement(1)]
+			public Vector2 WorldAnchor
+			{
+				get { return this.worldAnchor; }
+				set { this.worldAnchor = value; this.UpdateJoint(); }
+			}
+			/// <summary>
+			/// [GET / SET] The damping ratio. Zero means "no damping", one means "critical damping".
+			/// </summary>
+			[EditorHintRange(0.0f, 1.0f)]
+			public float DampingRatio
+			{
+				get { return this.dampingRatio; }
+				set { this.dampingRatio = value; this.UpdateJoint(); }
+			}
+			/// <summary>
+			/// [GET / SET] The mass spring damper frequency in hertz.
+			/// </summary>
+			public float Frequency
+			{
+				get { return this.frequency; }
+				set { this.frequency = value; this.UpdateJoint(); }
+			}
+			/// <summary>
+			/// [GET / SET] The target distance between local and world anchor
+			/// </summary>
+			[EditorHintIncrement(1)]
+			public float TargetDistance
+			{
+				get { return this.length; }
+				set { this.length = value; this.UpdateJoint(); }
+			}
+
+
+			public FixedDistanceJointInfo() {}
+
+			protected override Joint CreateJoint(Body bodyA, Body bodyB)
+			{
+				return bodyA != null ? JointFactory.CreateFixedDistanceJoint(Scene.PhysicsWorld, bodyA, Vector2.Zero, Vector2.Zero) : null;
+			}
+			internal override void UpdateJoint()
+			{
+				base.UpdateJoint();
+				if (this.joint == null) return;
+
+				FixedDistanceJoint j = this.joint as FixedDistanceJoint;
+				j.WorldAnchorB = PhysicsConvert.ToPhysicalUnit(this.worldAnchor);
+				j.LocalAnchorA = GetFarseerPoint(this.ColliderA, this.localAnchor);
+				j.DampingRatio = this.dampingRatio;
+				j.Frequency = this.frequency;
+				j.Length = PhysicsConvert.ToPhysicalUnit(this.length);
+			}
+
+			protected override void CopyTo(JointInfo target)
+			{
+				base.CopyTo(target);
+				FixedDistanceJointInfo c = target as FixedDistanceJointInfo;
+				c.worldAnchor = this.worldAnchor;
+				c.localAnchor = this.localAnchor;
+				c.dampingRatio = this.dampingRatio;
+				c.frequency = this.frequency;
+				c.length = this.length;
+			}
+		}
+
+		/// <summary>
 		/// "Welds" two Colliders together so they share a common point and relative angle.
 		/// </summary>
 		[Serializable]
 		public sealed class WeldJointInfo : JointInfo
 		{
-			private Vector2 localPointA	= Vector2.Zero;
-			private	Vector2	localPointB	= Vector2.Zero;
-			private	float	refAngle	= 0.0f;
+			private Vector2 localAnchorA	= Vector2.Zero;
+			private	Vector2	localAnchorB	= Vector2.Zero;
+			private	float	refAngle		= 0.0f;
 			
 
 			public override bool DualJoint
@@ -223,18 +317,18 @@ namespace Duality.Components
 			/// <summary>
 			/// [GET / SET] The welding point, locally to the first object.
 			/// </summary>
-			public Vector2 LocalPointA
+			public Vector2 LocalAnchorA
 			{
-				get { return this.localPointA; }
-				set { this.localPointA = value; this.UpdateJoint(); }
+				get { return this.localAnchorA; }
+				set { this.localAnchorA = value; this.UpdateJoint(); }
 			}
 			/// <summary>
 			/// [GET / SET] The welding point, locally to the second object.
 			/// </summary>
-			public Vector2 LocalPointB
+			public Vector2 LocalAnchorB
 			{
-				get { return this.localPointB; }
-				set { this.localPointB = value; this.UpdateJoint(); }
+				get { return this.localAnchorB; }
+				set { this.localAnchorB = value; this.UpdateJoint(); }
 			}
 			/// <summary>
 			/// [GET / SET] The relative angle both objects need to keep.
@@ -256,8 +350,8 @@ namespace Duality.Components
 				if (this.joint == null) return;
 
 				WeldJoint j = this.joint as WeldJoint;
-				j.LocalAnchorA = GetFarseerPoint(this.ColliderA, this.localPointA);
-				j.LocalAnchorB = GetFarseerPoint(this.ColliderB, this.localPointB);
+				j.LocalAnchorA = GetFarseerPoint(this.ColliderA, this.localAnchorA);
+				j.LocalAnchorB = GetFarseerPoint(this.ColliderB, this.localAnchorB);
 				j.ReferenceAngle = this.refAngle;
 			}
 
@@ -265,8 +359,8 @@ namespace Duality.Components
 			{
 				base.CopyTo(target);
 				WeldJointInfo c = target as WeldJointInfo;
-				c.localPointA = this.localPointA;
-				c.localPointB = this.localPointB;
+				c.localAnchorA = this.localAnchorA;
+				c.localAnchorB = this.localAnchorB;
 				c.refAngle = this.refAngle;
 			}
 		}
