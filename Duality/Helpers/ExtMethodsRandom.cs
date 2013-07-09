0759ef3d124ca4ad07386ffb65e9b219070371e0
mail@fetzenet.de <mail@fetzenet.de@879bfe85-3099-8c59-a0db-7ea583510f3d>
2013-07-09 07:49:18 +0000

    Tweaks --> Tweaked Random extension methods
    git-svn-id: http://duality.googlecode.com/svn/trunk@856 879bfe85-3099-8c59-a0db-7ea583510f3d

--- Duality/Helpers/ExtMethodsRandom.cs
+++ Duality/Helpers/ExtMethodsRandom.cs
@@ -306,17 +306,6 @@ namespace Duality
 		/// <param name="r"></param>
 		/// <param name="values"></param>
 		/// <returns></returns>
-		public static T OneOf<T>(this Random r, params T[] values)
-		{
-			return values[r.Next(values.Length)];
-		}
-		/// <summary>
-		/// Returns one randomly selected element.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="r"></param>
-		/// <param name="values"></param>
-		/// <returns></returns>
 		public static T OneOf<T>(this Random r, IEnumerable<T> values)
 		{
 			return values.ElementAt(r.Next(values.Count()));
